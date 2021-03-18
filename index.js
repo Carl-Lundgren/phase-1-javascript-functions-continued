6b3a367ca22c222082f8e79d6f02daec25586bc7
Carl <lundgren.e.carl@gmail.com>
2021-03-17 17:17:52 -0700

    commit tree 27fae78d16418133abcf521a11621bbb7fb03dca

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(event = "roller-skate") {
+    return `This Saturday, I want to ${event}!`;
+}
+
+const mondayWork = function(event = 'go to the office') {
+    return `This Monday, I will ${event}.`;
+}
+
+function wrapAdjective(wrap = '*') {
+    return (function(adj = special) {
+        return `You are ${wrap}${adj}${wrap}!`;
+    }); 
+}
